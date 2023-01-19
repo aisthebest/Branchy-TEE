09f171f59b574b978cf49747082627ff1e1f766a
Yulong Wang <wangyulonga@gmail.com>
2023-01-19 17:25:25 +0800

    Add files via upload

---
+++ SecEDIF-alexnet-native/predict.py
@@ -0,0 +1,108 @@
+import os
+import torch
+import torch.nn as nn
+import argparse
+import time
+#import torch.backends.cudnn as cudnn
+from PIL import Image
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Test')
+    parser.add_argument('--cuda', action='store_true', default=False, help=' use GPU?')
+    parser.add_argument('--batch-size', default=64, type=int, help="Batch Size for Test")
+    parser.add_argument('--num-workers', default=2, type=int, help='num-workers')
+    parser.add_argument('--net', type=str, choices=['LeNet5', 'AlexNet', 'VGG16', 'VGG19', 'ResNet18', 'ResNet34',
+                                                    'DenseNet', 'MobileNetv1', 'MobileNetv2'], default='AlexNet',
+                        help='net type')
+    args = parser.parse_args()
+
+    # Model
+    print('==> Building model..')
+    if args.net == 'VGG16':
+        from VGG import VGG
+
+        net = VGG('VGG16')
+    elif args.net == 'VGG19':
+        from VGG import VGG
+
+        net = VGG('VGG19')
+    elif args.net == 'ResNet18':
+        from ResNet import ResNet18
+
+        net = ResNet18()
+    elif args.net == 'ResNet34':
+        from ResNet import ResNet34
+
+        net = ResNet34()
+    elif args.net == 'LeNet5':
+        from LeNet5 import LeNet5
+
+        net = LeNet5()
+    elif args.net == 'AlexNet':
+        from AlexNet import AlexNet
+
+        net = AlexNet()
+    elif args.net == 'DenseNet':
+        from DenseNet import densenet_cifar
+
+        net = densenet_cifar()
+    elif args.net == 'MobileNetv1':
+        from MobileNetv1 import MobileNet
+
+        net = MobileNet()
+    elif args.net == 'MobileNetv2':
+        from MobileNetv2 import MobileNetV2
+
+        net = MobileNetV2()
+
+    if args.cuda and torch.cuda.is_available():
+        device = 'cuda'
+        net = torch.nn.DataParallel(net)
+    else:
+        device = 'cpu'
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/{}_ckpt.pth'.format(args.net))
+    net.load_state_dict(checkpoint['net'])
+    net.eval()
+
+    img = Image.open("./airplane.jpg").convert('RGB')  # 读取图像
+    trans = transforms.Compose([transforms.Resize((32, 32)),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                                                     std=(0.5, 0.5, 0.5)),
+                                ])
+
+    img = trans(img)
+    img = img.to(device)
+    img = img.unsqueeze(0)
+    # 扩展后，为[1，1，28，28]
+    load = time.perf_counter()
+    runTime = load - start
+
+    print("模型加载时间：", runTime, "秒")
+    output = net(img)
+    prob = F.softmax(output, dim=1)  # prob是10个分类的概率
+    print("概率", prob)
+    value, predicted = torch.max(output.data, 1)
+    print("类别", predicted.item())
+    print(value)
+    pred_class = classes[predicted.item()]
+    print("分类", pred_class)
+    end = time.perf_counter()
+    runTime = end - start
+    runTime_ms = runTime * 1000
+    # 输出运行时间
+    print("运行时间：", runTime, "秒")
+    print("运行时间：", runTime_ms, "毫秒")
---
+++ SecEDIF-vgg16-native/VGG.py
@@ -0,0 +1,65 @@
+'''
+VGG11,13,16,19 in pytorch
+'''
+
+#from turtle import forward
+import torch
+import torch.nn as nn
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+class VGG(nn.Module):
+    def __init__(self, vggname = 'VGG16',num_classes=10, init_weights=True):
+        super(VGG,self).__init__()
+        self.features = self._make_layers(cfg[vggname])
+        self.classifier = nn.Linear(512,num_classes)
+    
+    def _make_layers(self,cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M': # 最大池化层
+                layers += [nn.MaxPool2d(kernel_size=2,stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels,out_channels=x,kernel_size=3,padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1,stride=1)]
+        return nn.Sequential(*layers)
+        if init_weights:
+            self. _initialize_weight()
+    def forward(self,x):
+        x = self.features(x)
+        x = x.view(x.size()[0],-1)
+        x = self.classifier(x)
+        return x
+    # 初始化参数
+    def _initialize_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # xavier is used in VGG's paper
+                nn.init.xavier_normal_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+def test():
+    net = VGG('VGG19')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+    from torchinfo import summary
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    net = net.to(device)
+    summary(net,(2,3,32,32))
+    
+# test()
